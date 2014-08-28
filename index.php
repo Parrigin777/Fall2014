795bef91e49829be91fe36c5ade5b071908f0c02
unknown <n02644015@LC112PC09.active.newpaltz.edu>
2014-08-28 10:37:39 -0400

    First Commit in Aptana

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>index</title>
+		
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>This is our first file</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by n02644015
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
